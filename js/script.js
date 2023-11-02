c237e7e878386f2c38a3da07a41d7c862b5fa4fb
Hycarus <mirkomarasco08@gmail.com>
2023-11-02 17:12:27 +0100

    added function active index

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 import {contacts} from './data.js'
+import {getIndex} from './utility.js'
 const {createApp} = Vue
 createApp({
 // contiene tutti i dati / le variabili 
 data(){
     return {
         contacts: contacts,
+        activeContact: 1,
     }
 },
 // contiene le funzioni e i metodi
 methods: {
+},
+computed: {
+    activeIndex(){
+        return getIndex(this.activeContact, this.contacts)
+    }
 }
 }).mount('#app')
