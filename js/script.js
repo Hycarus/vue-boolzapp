0d420d33641e775a6b24311da8539e5a05f13b54
Hycarus <mirkomarasco08@gmail.com>
2023-11-02 18:36:26 +0100

    added function sendmessage and fix broken layout

--- js/script.js
+++ js/script.js
@@ -7,14 +7,23 @@ data(){
     return {
         contacts: contacts,
         activeContact: 1,
+        activeMessage: '',
     }
 },
 // contiene le funzioni e i metodi
 methods: {
     isActive(id){
         return id === this.activeContact ? true : false;
-    }
+    },
+    sendMessage(){
+        this.contacts[this.activeIndex].messages.push({
+            date: new Date().toLocaleString(),
+            message: this.activeMessage,
+            status:'sent'
+        })
+        this.activeMessage = ''
 
+    }
 },
 computed: {
     activeIndex(){
