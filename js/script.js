5bc5775b891708302a44de8a62f307cee5a8b5b0
Hycarus <mirkomarasco08@gmail.com>
2023-11-02 13:22:11 +0100

    created scaffholding

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import {contacts} from './data.js'
+const {createApp} = Vue
+createApp({
+// contiene tutti i dati / le variabili 
+data(){
+    return {
+    }
+},
+// contiene le funzioni e i metodi
+methods: {
+}
+}).mount('#app')
